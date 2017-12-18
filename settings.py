b8fee5b2888ab8bd3210d58e9f767ceb27b91654
kimbernator <groverperson@gmail.com>
2017-12-17 18:34:05 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ sort_terms = "date_submission,ASC"
 limit = 10000
 api_url = f"https://publicapi.fcc.gov/ecfs/filings?api_key={api_key}&proceedings.name={proceeding}&submissiontype.description=COMMENT&sort={sort_terms}"
 
-
 # Database settings
 db_host = '192.168.1.109'
 db_user = ''
