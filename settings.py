0edf73b4350bde090ad0d436187877133940ba3e
kimbernator <groverperson@gmail.com>
2017-12-17 18:26:18 -0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Query customizations
+api_key = ""
+proceeding = "17-108"
+sort_terms = "date_submission,ASC"
+limit = 10000
+api_url = f"https://publicapi.fcc.gov/ecfs/filings?api_key={api_key}&proceedings.name={proceeding}&submissiontype.description=COMMENT&sort={sort_terms}"
+
+
+# Database settings
+db_host = '192.168.1.109'
+db_user = ''
+db_password = ''
+db_schema = ''
