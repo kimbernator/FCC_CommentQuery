5cdcf2cdb9d3a7bf1f67a6cbfe5b74e6a0cf0cb1
kimbernator <groverperson@gmail.com>
2017-12-27 20:59:41 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 # Query customizations
-api_key = ""
-proceeding = "17-108"
-sort_terms = "date_submission,ASC"
-limit = 10000
+api_key = "TBTYBAOTWRtUNy0JU5WDqAE8T8ZO5ZNGddBPUcvK"
+proceeding = "17-108" # "Restoring Internet Freedom"
+sort_terms = "date_submission,ASC" # field,(ASC|DESC)
+limit = 10000 # 10,000 is the typical max, anything more will default to 25
 api_url = f"https://publicapi.fcc.gov/ecfs/filings?api_key={api_key}&proceedings.name={proceeding}&submissiontype.description=COMMENT&sort={sort_terms}"
+
 
 # Database settings
 db_host = '192.168.1.109'
